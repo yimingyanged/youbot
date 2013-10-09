62a9a81b09edbf6e3f7c5e85501433c6c814ada2
youbot <youbot@KukaYouBot.(none)>
2013-10-09 15:54:24 +0100

    cleanup

---
+++ workspaces/youbot/src/youbot_oodl/README.md
@@ -0,0 +1,15 @@
+youbot_common [![Build Status](https://api.travis-ci.org/WPI-RAIL/youbot_oodl.png)](https://travis-ci.org/WPI-RAIL/youbot_oodl)
+===========
+
+#### ROS Wrapper for the KUKA youBot Driver
+For full documentation, see [the ROS wiki](http://ros.org/wiki/youbot_common).
+
+[Doxygen](http://ros.org/doc/groovy/api/youbot_common/html/) files can be found on the ROS wiki.
+
+This project is a fork of the [original youbot-ros-pkg package](https://github.com/youbot/youbot-ros-pkg) for use with [catkin](http://www.ros.org/wiki/catkin) and the [ROS release process](http://www.ros.org/wiki/bloom/). The [youbot_driver](https://github.com/WPI-RAIL/youbot_driver) must have been installed from the latest available sources for this package to correctly build.
+
+### License
+youbot_common is released under a dual-license: LGPL 2.1 and BSD. The dual-license implies that users of this code may choose which terms they prefer. For full terms and conditions, see the [LICENSE](LICENSE) file.
+
+### Authors
+See the [AUTHORS.md](AUTHORS.md) file for a full list of contributors.
---
+++ workspaces/youbot/src/youbot_oodl/src/youbot_wrapper.cpp
@@ -0,0 +1,214 @@
+/*****************************************************************************
+ * Copyright (c) 2011
+ * Locomotec
+ *
+ * Author:
+ * Sebastian Blumenthal
+ *
+ *
+ * This software is published under a dual-license: GNU Lesser General Public
+ * License LGPL 2.1 and BSD license. The dual-license implies that users of this
+ * code may choose which terms they prefer.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of Locomotec nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version or the BSD license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL and the BSD license for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL and BSD license along with this program.
+ *
+ ******************************************************************************/
+
+
+#include <YouBotOODLWrapper.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <math.h>
+#include <unistd.h>
+
+#include "std_msgs/Bool.h"
+#include "std_srvs/Empty.h"
+#include "geometry_msgs/Twist.h"
+
+bool has_received_heartbeat=false;
+int heartbeat_timeout=0;
+const int heartbeat_timeout_max=50;
+bool bad=false;
+
+ros::Time last_heartbeat_time;
+
+void heartbeatCallback(const std_msgs::Bool::ConstPtr& msg)
+{
+  ros::Time now = ros::Time::now();
+  if (!has_received_heartbeat) ROS_INFO("Got first heartbeat");
+  else {
+    //ROS_INFO_STREAM("Got heartbeat " << heartbeat_timeout << " ticks after last one");
+    double secs = (now-last_heartbeat_time).toSec();
+    if (secs>=heartbeat_timeout_max*0.01)
+    {
+      ROS_WARN_STREAM("Got heartbeat " << (now-last_heartbeat_time).toSec() << " seconds after last, reconnecting...");
+      //heartbeat is late so we have timed out already
+      //call reconnect and reset state
+      std_srvs::Empty::Request req;
+      std_srvs::Empty::Response res;
+      ros::service::call("/reconnect",req,res);
+      printf("Reconnected\r\n");
+      bad=false;
+      now=ros::Time::now();
+      
+    }
+    else
+    {
+      ROS_INFO_STREAM("Got heartbeat " << (now-last_heartbeat_time).toSec() << " seconds after last");
+    }
+  }
+
+  has_received_heartbeat=true;
+  heartbeat_timeout=0;
+  last_heartbeat_time=now;
+}
+
+void checkHeartbeat(youBot::YouBotOODLWrapper *youBot) {
+  //printf("AWESOME\r\n");
+  if (bad) {
+      ROS_INFO("Stopping motors...");
+      //kill motors
+      //&YouBotOODLWrapper::switchOffBaseMotorsCallback, this
+      //bool YouBotOODLWrapper::switchOffBaseMotorsCallback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+      std_srvs::Empty::Request req;
+      std_srvs::Empty::Response res;
+
+      youBot->switchOffBaseMotorsCallback(req, res);
+      //ros::service::call("arm_1/switchOffMotors", empty);
+      //ros::service::call("base/switchOffMotors", empty); 
+
+      ROS_INFO("Stopped motors");
+  }
+
+  if (has_received_heartbeat && !bad) {
+    heartbeat_timeout++;
+      
+    if (heartbeat_timeout > heartbeat_timeout_max)
+    {
+      ROS_INFO("HEARTBEAT TIMED OUT!");
+      bad = true;
+    }
+  }
+}
+
+
+int main(int argc, char **argv)
+{
+
+  youbot::Logger::toConsole = false;
+  youbot::Logger::toFile = false;
+  youbot::Logger::toROS = true;
+  ros::init(argc, argv, "youbot_wrapper");
+  ros::NodeHandle n;
+  youBot::YouBotOODLWrapper youBot(n);
+  std::vector<std::string> armNames;
+
+  /* configuration */
+  bool youBotHasBase;
+  bool youBotHasArms;
+  bool runEstopNetworkMonitor;
+  double youBotDriverCycleFrequencyInHz;  //the driver recives commands and publishes them with a fixed frequency
+  n.param("youBotHasBase", youBotHasBase, true);
+  n.param("youBotHasArms", youBotHasArms, true);
+  n.param("runNetworkEstopper", runEstopNetworkMonitor, false);
+  n.param("youBotDriverCycleFrequencyInHz", youBotDriverCycleFrequencyInHz, 50.0);
+
+  //get the config file path from an environment variable
+  char* configLocation = getenv("YOUBOT_CONFIG_FOLDER_LOCATION");
+  if (configLocation == NULL)
+    throw std::runtime_error("youbot_wrapper.cpp: Could not find environment variable YOUBOT_CONFIG_FOLDER_LOCATION");
+
+  n.param < std::string
+      > ("youBotConfigurationFilePath", youBot.youBotConfiguration.configurationFilePath, configLocation);
+
+  n.param < std::string > ("youBotBaseName", youBot.youBotConfiguration.baseConfiguration.baseID, "youbot-base");
+
+  // Retrieve all defined arm names from the launch file params
+  int i = 1;
+  std::stringstream armNameParam;
+  armNameParam << "youBotArmName" << i; // youBotArmName1 is first checked param... then youBotArmName2, etc.
+  while (n.hasParam(armNameParam.str()))
+  {
+    std::string armName;
+    n.getParam(armNameParam.str(), armName);
+    armNames.push_back(armName);
+    ROS_DEBUG("Added arm %s to list of armsNames", armNameParam.str().c_str());
+    armNameParam.str("");
+    armNameParam << "youBotArmName" << (++i);
+
+  }
+
+  ros::ServiceServer reconnectService = n.advertiseService("reconnect", &youBot::YouBotOODLWrapper::reconnectCallback,
+                                                           &youBot);
+
+  ros::Subscriber heartbeatSubscriber = n.subscribe("youbot_network_heartbeat", 1, heartbeatCallback);
+
+
+
+  ROS_ASSERT((youBotHasBase == true) || (youBotHasArms == true)); // At least one should be true, otherwise nothing to be started.
+  if (youBotHasBase == true)
+  {
+    youBot.initializeBase(youBot.youBotConfiguration.baseConfiguration.baseID);
+  }
+
+  if (youBotHasArms == true)
+  {
+    std::vector<std::string>::iterator armNameIter;
+    for (armNameIter = armNames.begin(); armNameIter != armNames.end(); ++armNameIter)
+    {
+      youBot.initializeArm(*armNameIter);
+      ROS_DEBUG("Added arm \"%s\" to list of armsNames", armNameParam.str().c_str());
+    }
+  }
+
+  /* coordination */
+  ros::Rate rate(youBotDriverCycleFrequencyInHz); //Input and output at the same time... (in Hz)
+  while (n.ok())
+  {
+    ros::spinOnce();
+
+    //if (runNetworkEstopper)
+      checkHeartbeat(&youBot);
+
+    youBot.computeOODLSensorReadings();
+    youBot.publishOODLSensorReadings();
+    youBot.publishArmAndBaseDiagnostics(2.0);    //publish only every 2 seconds
+    rate.sleep();
+
+    //automatically attempt a reconnect if ethercat dies
+    if (!youBot.youBotConfiguration.isEtherCATOkay()) {
+      std_srvs::Empty::Request req;
+      std_srvs::Empty::Response res;
+      youBot.reconnectCallback(req,res);
+    }
+
+  }
+
+  youBot.stop();
+
+  return 0;
+}
