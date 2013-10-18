7cd096b3eb0cc54d3379b4c4a5b2abf37a5e3f9c
raha <raha@Raha-linux>
2013-10-18 09:02:48 +0100

    modified the local trajectory test client
    Now you specify the parameters for the robot, and can specifically
    override the values for joint 5.

--- workspaces/raha/src/youbot_ed_trajectory_action_server/src/trajectory_test.cpp
+++ workspaces/raha/src/youbot_ed_trajectory_action_server/src/trajectory_test.cpp
@@ -60,6 +60,39 @@ public:
 		traj_client_->sendGoal(goal);
 	}
 
+	control_msgs::FollowJointTrajectoryGoal joint5test(int poseId)
+	{
+		control_msgs::FollowJointTrajectoryGoal goal;
+
+		// First, the joint names, which apply to all waypoints
+		goal.trajectory.joint_names.push_back("arm_joint_1");
+		goal.trajectory.joint_names.push_back("arm_joint_2");
+		goal.trajectory.joint_names.push_back("arm_joint_3");
+		goal.trajectory.joint_names.push_back("arm_joint_4");
+		goal.trajectory.joint_names.push_back("arm_joint_5");
+
+		// We will have two waypoints in this goal trajectory
+		goal.trajectory.points.resize(1);
+
+		int ind  = 0;
+		if(poseId == 0)
+		{
+			// First point
+			setFirstPoint(goal, ind, 2.0);
+		}
+		else
+		{
+			// Second trajectory point
+//			ind++;
+			setSecondPoint(goal, ind, 2.0);
+//			goal.trajectory.points[ind].positions[4] = 2.0;
+		}
+
+//		ROS_INFO("Joint 5 set-point is: %4.2f", goal.trajectory.points[ind].positions[4]);
+
+		//we are done; return the goal
+		return goal;
+	}
 	//! Generates a simple trajectory with two waypoints, used as an example
 	/*! Note that this trajectory contains two waypoints, joined together
 	 as a single trajectory. Alternatively, each of these waypoints could
@@ -87,7 +120,7 @@ public:
 
 		// Second trajectory point
 		ind++;
-		setFirstPoint(goal, ind, 8.0);
+		setSecondPoint(goal, ind, 8.0);
 
 		// First point
 		ind++;
@@ -102,15 +135,15 @@ public:
 
 		// First trajectory point
 					// Positions
-		ROS_INFO("creating 1st point");
+//		ROS_INFO("creating 1st point");
 		goal.trajectory.points[ind].positions.resize(5);
 		goal.trajectory.points[ind].positions[0] = 0.25;
 		goal.trajectory.points[ind].positions[1] = 0.30;
 		goal.trajectory.points[ind].positions[2] =-0.45;
 		goal.trajectory.points[ind].positions[3] = 0.14;
-		goal.trajectory.points[ind].positions[4] = 1.4;
+		goal.trajectory.points[ind].positions[4] = 0.80;
 		// Velocities
-		ROS_INFO("1: vels");
+//		ROS_INFO("1: vels");
 		goal.trajectory.points[ind].velocities.resize(5);
 		for (size_t j = 0; j < 5; ++j) {
 			goal.trajectory.points[ind].velocities[j] = 0.00001;
@@ -129,13 +162,13 @@ public:
 	{
 
 		// Positions
-		ROS_INFO("creating 2nd point");
+//		ROS_INFO("creating 2nd point");
 		goal.trajectory.points[ind].positions.resize(5);
-		goal.trajectory.points[ind].positions[0] = 0.5;
+		goal.trajectory.points[ind].positions[0] = 1.5;
 		goal.trajectory.points[ind].positions[1] = 0.30;
 		goal.trajectory.points[ind].positions[2] =-0.45;
-		goal.trajectory.points[ind].positions[3] = 0.14;
-		goal.trajectory.points[ind].positions[4] = 4.6;
+		goal.trajectory.points[ind].positions[3] = 0.34;
+		goal.trajectory.points[ind].positions[4] = 1.80;
 		// Velocities
 		goal.trajectory.points[ind].velocities.resize(5);
 		for (size_t j = 0; j < 5; ++j) {
@@ -208,6 +241,19 @@ void spinThread() {
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "robot_crtl_action_client_2");
+
+	int poseId = 0;
+	float setpoint = -1.0;
+	if(argc>1)
+		poseId= atoi(argv[1]);
+	ROS_INFO("Using pose id %d",poseId);
+
+	if(argc>2)
+	{
+		setpoint = atof(argv[2]);
+		ROS_INFO("Using setpoint id %f",setpoint);
+	}
+
 	// Acces to the trajoxtory generatror
 	RobotArm arm(false);
 	RobotGripper gripper;
@@ -220,16 +266,19 @@ int main(int argc, char **argv) {
 	boost::thread spin_thread(&spinThread);
 
 	// waiting for servers
-	ROS_INFO("Waiting for action servers to start.");
+	ROS_DEBUG("Waiting for action servers to start.");
 	acArm.waitForServer();
-	ROS_INFO("FollowJointTrajectoryAction server started.");
+	ROS_DEBUG("FollowJointTrajectoryAction server started.");
 	acGripper.waitForServer();
-	ROS_INFO("GripperCommandAction server started.");
-	ROS_INFO("All action servers started, sending goals	.");
+	ROS_DEBUG("GripperCommandAction server started.");
+	ROS_DEBUG("All action servers started, sending goals	.");
 
 	// send a goal to the arm action
 	control_msgs::FollowJointTrajectoryGoal armGoal;
-	armGoal = arm.armExtensionTrajectory();
+//	armGoal = arm.armExtensionTrajectory();
+	armGoal = arm.joint5test(poseId);
+	if(setpoint > 0.0)
+		armGoal.trajectory.points[0].positions[4] = setpoint;
 	ROS_INFO("Arm goal created, now sending...");
 	acArm.sendGoal(armGoal);
 
@@ -246,35 +295,35 @@ int main(int argc, char **argv) {
 	}
 
 
-	// send a goal to the arm action
-	control_msgs::GripperCommandGoal gripperGoal;
-	gripperGoal = gripper.getOpenGripperGoal();
-	ROS_INFO("Open gripper goal created, now sending");
-	acGripper.sendGoal(gripperGoal);
-	//wait for the action to return
-	finished_before_timeout = acGripper.waitForResult(ros::Duration(10.0));
-	if (finished_before_timeout) {
-		actionlib::SimpleClientGoalState state = acGripper.getState();
-		ROS_INFO("Open gripper action finished: %s", state.toString().c_str());
-	} else
-	{
-		ROS_INFO("Open gripper action did not finish before the time out -> cancelling all goals.");
-		acGripper.cancelAllGoals();
-	}
-
-	gripperGoal = gripper.getCloseGripperGoal();
-	ROS_INFO("Close gripper goal created, now sending");
-	acGripper.sendGoal(gripperGoal);
-	//wait for the action to return
-	finished_before_timeout = acGripper.waitForResult(ros::Duration(10.0));
-	if (finished_before_timeout) {
-		actionlib::SimpleClientGoalState state = acGripper.getState();
-		ROS_INFO("Close gripper action finished: %s", state.toString().c_str());
-	} else
-	{
-		ROS_INFO("Close gripper action did not finish before the time out -> canceling all goals.");
-		acGripper.cancelAllGoals();
-	}
+//	// send a goal to the arm action
+//	control_msgs::GripperCommandGoal gripperGoal;
+//	gripperGoal = gripper.getOpenGripperGoal();
+//	ROS_INFO("Open gripper goal created, now sending");
+//	acGripper.sendGoal(gripperGoal);
+//	//wait for the action to return
+//	finished_before_timeout = acGripper.waitForResult(ros::Duration(10.0));
+//	if (finished_before_timeout) {
+//		actionlib::SimpleClientGoalState state = acGripper.getState();
+//		ROS_INFO("Open gripper action finished: %s", state.toString().c_str());
+//	} else
+//	{
+//		ROS_INFO("Open gripper action did not finish before the time out -> cancelling all goals.");
+//		acGripper.cancelAllGoals();
+//	}
+//
+//	gripperGoal = gripper.getCloseGripperGoal();
+//	ROS_INFO("Close gripper goal created, now sending");
+//	acGripper.sendGoal(gripperGoal);
+//	//wait for the action to return
+//	finished_before_timeout = acGripper.waitForResult(ros::Duration(10.0));
+//	if (finished_before_timeout) {
+//		actionlib::SimpleClientGoalState state = acGripper.getState();
+//		ROS_INFO("Close gripper action finished: %s", state.toString().c_str());
+//	} else
+//	{
+//		ROS_INFO("Close gripper action did not finish before the time out -> canceling all goals.");
+//		acGripper.cancelAllGoals();
+//	}
 
 	// shutdown the node and join the thread back before exiting
 	ROS_INFO("Shutting down..");
